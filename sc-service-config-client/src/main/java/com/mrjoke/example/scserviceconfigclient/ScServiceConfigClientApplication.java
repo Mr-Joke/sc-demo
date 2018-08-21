7f9418c8471c6d16cb0e0d5fc08c7cbdbd994800
Mrzhou <mr.jokein2016@gmail.com>
2018-08-21 23:07:53 +0800

    客户端拉去配置加入消息总线通知更新配置，尚未成功

--- sc-service-config-client/src/main/java/com/mrjoke/example/scserviceconfigclient/ScServiceConfigClientApplication.java
+++ sc-service-config-client/src/main/java/com/mrjoke/example/scserviceconfigclient/ScServiceConfigClientApplication.java
@@ -2,8 +2,12 @@ package com.mrjoke.example.scserviceconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
 public class ScServiceConfigClientApplication {
 
 	public static void main(String[] args) {
